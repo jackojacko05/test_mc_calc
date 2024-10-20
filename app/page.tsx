b2bd34753dfa43e8d05897f35f5753e57cdbe2bc
jackojacko05 <jacko05.gc+ghjacko@gmail.com>
2024-10-20 23:16:39 +0900

    index.tsxの削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { MedalCalculatorComponent } from "@/components/medal-calculator";
 
 export default function Home() {
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
